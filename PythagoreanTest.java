a776b991ccdb62952f275c6c10f72115eac5e2cd
bhansen0091 <bhansen0091@yahoo.com>
2021-04-05 13:37:25 -0500

    initial commit

---
+++ PythagoreanTest.java
@@ -0,0 +1,8 @@
+
+public class PythagoreanTest {
+
+    public static void main(String[] args) {
+        Pythagorean p = new Pythagorean();
+        System.out.println(p.calculateHypotenuse(3, 4));
+    }    
+}
